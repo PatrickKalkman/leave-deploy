e64042ce74647034b3d8fe6db53b545f6c160e54
Patrick Kalkman <patrick.kalkman@exlrt.com>
2021-02-26 22:32:03 +0100

    configured networks

--- prediction_service/main.py
+++ prediction_service/main.py
@@ -63,4 +63,4 @@ async def hello():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, access_log=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, access_log=True)
